0692222ddbf4afe20c27e5015b4119901d410b6c
AbhisarSaraswat <abhisarsaraswat@gmail.com>
2018-09-29 01:52:29 +0530

    These are the problems with their respective solutions of hackerrank

---
+++ Loops/Solution.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    for var in range (n):
+        print(var * var)
+        var = var+1
---
+++ Print function/Solution.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    var=1
+    for var in range (n):
+        print (var+1,end="")
+        var = var +1
